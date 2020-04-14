ba04998f67fa51238ea70a886a5f92b807c584bc
Lucas Di Fini <difinile@gmail.com>
2020-04-14 16:19:44 -0400

    second commit, adding images,css, and more

--- start-bootstrap/src/App.js
+++ start-bootstrap/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <a class="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
-        <i class="fas fa-bars"></i>
+        <i class="fa fa-bars"></i>
       </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav text-uppercase ml-auto">
@@ -55,24 +55,24 @@ function App() {
       <div class="row text-center">
         <div class="col-md-4">
           <span class="fa-stack fa-4x">
-            <i class="fas fa-circle fa-stack-2x text-primary"></i>
-            <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
+            <i class="fa fa-circle fa-stack-2x text-primary"></i>
+            <i class="fa fa-shopping-cart fa-stack-1x fa-inverse"></i>
           </span>
           <h4 class="service-heading">E-Commerce</h4>
           <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
         </div>
         <div class="col-md-4">
           <span class="fa-stack fa-4x">
-            <i class="fas fa-circle fa-stack-2x text-primary"></i>
-            <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
+            <i class="fa fa-circle fa-stack-2x text-primary"></i>
+            <i class="fa fa-laptop fa-stack-1x fa-inverse"></i>
           </span>
           <h4 class="service-heading">Responsive Design</h4>
           <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
         </div>
         <div class="col-md-4">
           <span class="fa-stack fa-4x">
-            <i class="fas fa-circle fa-stack-2x text-primary"></i>
-            <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
+            <i class="fa fa-circle fa-stack-2x text-primary"></i>
+            <i class="fa fa-lock fa-stack-1x fa-inverse"></i>
           </span>
           <h4 class="service-heading">Web Security</h4>
           <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
@@ -94,7 +94,7 @@ function App() {
           <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
             <div class="portfolio-hover">
               <div class="portfolio-hover-content">
-                <i class="fas fa-plus fa-3x"></i>
+                <i class="fa fa-plus fa-3x"></i>
               </div>
             </div>
             <img class="img-fluid" src="img/portfolio/01-thumbnail.jpg" alt="" />
@@ -108,7 +108,7 @@ function App() {
           <a class="portfolio-link" data-toggle="modal" href="#portfolioModal2">
             <div class="portfolio-hover">
               <div class="portfolio-hover-content">
-                <i class="fas fa-plus fa-3x"></i>
+                <i class="fa fa-plus fa-3x"></i>
               </div>
             </div>
             <img class="img-fluid" src="img/portfolio/02-thumbnail.jpg" alt="" />
@@ -122,7 +122,7 @@ function App() {
           <a class="portfolio-link" data-toggle="modal" href="#portfolioModal3">
             <div class="portfolio-hover">
               <div class="portfolio-hover-content">
-                <i class="fas fa-plus fa-3x"></i>
+                <i class="fa fa-plus fa-3x"></i>
               </div>
             </div>
             <img class="img-fluid" src="img/portfolio/03-thumbnail.jpg" alt="" />
@@ -136,7 +136,7 @@ function App() {
           <a class="portfolio-link" data-toggle="modal" href="#portfolioModal4">
             <div class="portfolio-hover">
               <div class="portfolio-hover-content">
-                <i class="fas fa-plus fa-3x"></i>
+                <i class="fa fa-plus fa-3x"></i>
               </div>
             </div>
             <img class="img-fluid" src="img/portfolio/04-thumbnail.jpg" alt="" />
@@ -150,7 +150,7 @@ function App() {
           <a class="portfolio-link" data-toggle="modal" href="#portfolioModal5">
             <div class="portfolio-hover">
               <div class="portfolio-hover-content">
-                <i class="fas fa-plus fa-3x"></i>
+                <i class="fa fa-plus fa-3x"></i>
               </div>
             </div>
             <img class="img-fluid" src="img/portfolio/05-thumbnail.jpg" alt="" />
@@ -164,7 +164,7 @@ function App() {
           <a class="portfolio-link" data-toggle="modal" href="#portfolioModal6">
             <div class="portfolio-hover">
               <div class="portfolio-hover-content">
-                <i class="fas fa-plus fa-3x"></i>
+                <i class="fa fa-plus fa-3x"></i>
               </div>
             </div>
             <img class="img-fluid" src="img/portfolio/06-thumbnail.jpg" alt="" />
@@ -280,12 +280,12 @@ function App() {
               </li>
               <li class="list-inline-item">
                 <a href="#">
-                  <i class="fab fa-facebook-f"></i>
+                  <i class="fa fa-facebook-f"></i>
                 </a>
               </li>
               <li class="list-inline-item">
                 <a href="#">
-                  <i class="fab fa-linkedin-in"></i>
+                  <i class="fa fa-linkedin-in"></i>
                 </a>
               </li>
             </ul>
@@ -299,17 +299,17 @@ function App() {
             <ul class="list-inline social-buttons">
               <li class="list-inline-item">
                 <a href="#">
-                  <i class="fab fa-twitter"></i>
+                  <i class="fa fa-twitter"></i>
                 </a>
               </li>
               <li class="list-inline-item">
                 <a href="#">
-                  <i class="fab fa-facebook-f"></i>
+                  <i class="fa fa-facebook-f"></i>
                 </a>
               </li>
               <li class="list-inline-item">
                 <a href="#">
-                  <i class="fab fa-linkedin-in"></i>
+                  <i class="fa fa-linkedin-in"></i>
                 </a>
               </li>
             </ul>
@@ -323,17 +323,17 @@ function App() {
             <ul class="list-inline social-buttons">
               <li class="list-inline-item">
                 <a href="#">
-                  <i class="fab fa-twitter"></i>
+                  <i class="fa fa-twitter"></i>
                 </a>
               </li>
               <li class="list-inline-item">
                 <a href="#">
-                  <i class="fab fa-facebook-f"></i>
+                  <i class="fa fa-facebook-f"></i>
                 </a>
               </li>
               <li class="list-inline-item">
                 <a href="#">
-                  <i class="fab fa-linkedin-in"></i>
+                  <i class="fa fa-linkedin-in"></i>
                 </a>
               </li>
             </ul>
@@ -429,17 +429,17 @@ function App() {
           <ul class="list-inline social-buttons">
             <li class="list-inline-item">
               <a href="#">
-                <i class="fab fa-twitter"></i>
+                <i class="fa fa-twitter"></i>
               </a>
             </li>
             <li class="list-inline-item">
               <a href="#">
-                <i class="fab fa-facebook-f"></i>
+                <i class="fa fa-facebook-f"></i>
               </a>
             </li>
             <li class="list-inline-item">
               <a href="#">
-                <i class="fab fa-linkedin-in"></i>
+                <i class="fa fa-linkedin-in"></i>
               </a>
             </li>
           </ul>
